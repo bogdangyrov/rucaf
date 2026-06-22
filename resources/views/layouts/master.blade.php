d7584e377511a45afeaacf5e2eb5147a0da3f58f
Bogdan Gyrov <gyrovbogdan@mail.ru>
2026-06-22 17:00:48 +0300

    Снял no index, добавил верификацию яндекс

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="yandex-verification" content="f0f5e0c422ada7d3" />
+
     @include('layouts.components.seo')
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
