bf72d928f4d005a207ee70d02deae37edaf6fdd8
Bogdan Gyrov <gyrovbogdan@mail.ru>
2026-07-01 12:50:39 +0300

    Добавил генерацию sitemap

--- routes/console.php
+++ routes/console.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('queue:work --stop-when-empty --tries=3')->everyFiveMinutes();
+
+Schedule::command('app:generate-sitemap')->cron('0 3 */3 * *')
+    ->withoutOverlapping()
+    ->sendOutputTo(storage_path('logs/sitemap.log'));
+
 Schedule::command('backup:clean')->daily()->at('03:00');
 Schedule::command('backup:run')->daily()->at('03:30');
