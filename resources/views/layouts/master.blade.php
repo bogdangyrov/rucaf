991ec75d8966f2a8e05fcf5911ebd60f6c143333
Gyrov Bogdan <gyrovbogdangit@gmail.com>
2026-07-07 11:40:14 +0300

    Добавил favicon.ico

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="yandex-verification" content="f0f5e0c422ada7d3" />
+    <link rel="icon" href="{{ asset('favicon.ico')}}" type="image/x-icon">
 
     @include('layouts.components.seo')
 
